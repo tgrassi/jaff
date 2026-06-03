0c1ea8d2dddf0cf0ebc4aa1a19c29a1b57812370
XRByte <sarkar.anish.1001@gmail.com>
2026-06-04 00:22:43 +1000

    Updated jaff.db to contain information regarding cross-section tables

--- src/jaff/errors/__init__.py
+++ src/jaff/errors/__init__.py
@@ -1,3 +1,11 @@
 from ._parser import NotJaffFileError, ParserError, SympyJsonError
+from ._units import IncompatibleUnitsError, UnitsError, UnknownUnitError
 
-__all__ = ["NotJaffFileError", "ParserError", "SympyJsonError"]
+__all__ = [
+    "NotJaffFileError",
+    "ParserError",
+    "SympyJsonError",
+    "UnitsError",
+    "UnknownUnitError",
+    "IncompatibleUnitsError",
+]
